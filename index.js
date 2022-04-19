90d63917fbfdf01541de73ae30eef95df2f44d76
ChiThang-50Cent <chithang.nydo@gmail.com>
2022-04-19 20:31:51 +0700

    add db

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+
 const app = express();
+
+require('./setup/database')();
+
+app.listen(3000, () => {
+    console.log("Server is running at port 3000");
+});
