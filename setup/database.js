488878e1c3da1b1c74b786201dc9d71c031e323e
ChiThang-50Cent <chithang.nydo@gmail.com>
2022-05-01 09:03:34 +0700

    check

--- setup/database.js
+++ setup/database.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect('mongodb://localhost:27017/MindX');
+    //mongoose.connect('mongodb://localhost:27017/MindX');
+    mongoose.connect(
+        "mongodb+srv://thang:abc123456@cluster0.q0xp1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
     const db = mongoose.connection;
     db.on("error", console.error.bind(console, "connection error: "));
     db.once("open", function() {
         console.log("Connected successfully");
     });
-}
+};
